10e12c6702289295dbd03a71e5cb13acb1efa8cf
Iron Tony <lifegamecom1@gmail.com>
2020-06-22 10:01:23 +0300

    Added bottom nav

--- app/src/main/java/com/example/storagemanager/MainActivity.java
+++ app/src/main/java/com/example/storagemanager/MainActivity.java
@@ -7,23 +7,46 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.DataBindingUtil;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.example.storagemanager.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mBottomNav;
+    private NavController mNavController;
+    private AppBarConfiguration mAppBarConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        ActivityMainBinding binding = DataBindingUtil
+                .setContentView(this, R.layout.activity_main);
+
+        Toolbar toolbar = binding.toolbar;
+        mBottomNav = binding.bottomNavigationView;
+        mNavController = Navigation.findNavController(this, R.id.navHostFragment);
+        mAppBarConfig = new AppBarConfiguration
+                .Builder(R.id.allGoodsFragment, R.id.groupsFragment)
+                .build();
+
+        setSupportActionBar(binding.toolbar);
+
+        NavigationUI.setupWithNavController(toolbar, mNavController, mAppBarConfig);
+        NavigationUI.setupWithNavController(mBottomNav, mNavController);
+
+        setupDestinations();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
         return true;
     }
 
@@ -36,5 +59,14 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void setupDestinations() {
+        mNavController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (mAppBarConfig.getTopLevelDestinations().contains(destination.getId()))
+                mBottomNav.setVisibility(View.VISIBLE);
+            else
+                mBottomNav.setVisibility(View.GONE);
+        });
     }
 }
